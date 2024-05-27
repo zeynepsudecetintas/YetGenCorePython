ff8ab919f13d208f3b30d946873750044d955c44
Zeynep Sude Çetintaş <zeynepsudecetintas@hotmail.com>
2024-05-27 21:42:46 +0300

    İkinci hafta ödevi yapıldı.

---
+++ W2 - Eğitim Öncesi Uygulamaları.py
@@ -0,0 +1,8 @@
+# Birinci sorunun cevabı aşağıda:
+print("Hello World")
+# İkinci sorunun cevabı aşağıda:
+
+# Üçüncü sorunun cevabı aşağıda:
+## Bu soruda dairenin alanını hesaplayacağız.
+### dairenin alanı X = πr^2 formülü ile hesaplanır.
+
